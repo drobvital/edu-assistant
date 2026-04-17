2b727cbb614a82773a38d80eca8577118ef4fe10
Drobot Vitaly <vitalyi.drobot@yandex.ru>
2026-04-17 11:35:59 +0300

    Подключаемся к LLM

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+from dotenv import load_dotenv
 from edu_assistant.config import Config
+from edu_assistant.llm_client import get_llm_client
 
-#Create config file path
+# Load environment variables from .env file
+load_dotenv()
+
+# Prepare prompt for LLM
+INPUT_PROMPT = "Кто ты?"
+
+# Read config from YAML file
 config = Config.from_yaml_file("config.yml")
-print("Full config:", config)
-print("LLM model:", config.llms["api"].model)
+llm_config = config.llms["ollama"]
 
-system_prompt = config.render_system_instructions(role="history_tutor",template="tutor_quick_answer")
-print("System prompt:", system_prompt)
+# Create llm_client
+llm_client = get_llm_client(llm_config)
+
+# Вызов llm_client Responses API передав промпт как input
+response = llm_client.responses.create(
+    model = llm_config.model,
+    input = INPUT_PROMPT
+)
+ 
+
+print(response.output_text)
